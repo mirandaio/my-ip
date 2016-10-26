d3fe14a4a43f0095beb7776a56f68579d9d70802
Alfredo Miranda <alfredo@miranda.io>
2016-10-26 14:34:04 -0500

    Return IP to client

---
+++ index.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const koa = require('koa');
+const views = require('co-views');
+
+let app = koa();
+let render = views(path.join(__dirname, '/views'), {ext: 'ejs'});
+
+app.proxy = true;
+
+app.use(function *() {
+  this.body = yield render('index', {ip: this.request.ip});
+});
+
+app.listen(process.env.PORT || 8080);
