9339564f58ccece919ef788d62308d9498abf7da
Alfredo Miranda <alfredo@miranda.io>
2016-10-26 16:16:28 -0500

    Add favicon

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const koa = require('koa');
+const serve = require('koa-static');
 const views = require('co-views');
 
 let app = koa();
 let render = views(path.join(__dirname, '/views'), {ext: 'ejs'});
 
 app.proxy = true;
+
+app.use(serve('public'));
 
 app.use(function *() {
   this.body = yield render('index', {ip: this.request.ip});
